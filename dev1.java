310befb181d5b84be92d9ece50565f1a06d703ff
Meena <keerthimeena450@gmail.com>
2023-05-26 18:18:48 +0530

    this one

---
+++ dev1.java
@@ -0,0 +1,17 @@
+Class Signup
+{
+public static void main(String[] args)
+{
+    int a=2;
+    int b=4;
+    if(a>b)
+    {
+        System.out.println("java  not Installed");
+    }
+    else
+    {
+        System.out.println("java Installed");
+	a++;
+    }
+}
+}
